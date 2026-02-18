9778068963dba07576c37e453a7895b9e329686e
Saheed Ayomide <125904586+Luc-ay@users.noreply.github.com>
2026-02-18 21:33:51 +0100

    Refactor payment routes to use getTicketByTxRef

--- Backend Server/routes/payment.routes.js
+++ Backend Server/routes/payment.routes.js
@@ -3,18 +3,14 @@ import {
   initiatePayment,
   paymentRedirect,
   flutterwaveWebhook,
-  getAllPayments,
-  getAllTickets,
-  ticket,
+  getTicketByTxRef,
 } from '../controller/payment.controller.js'
 
 const router = express.Router()
 
 router.post('/initiate', initiatePayment)
 router.get('/redirect', paymentRedirect)
-router.post('/ticket', ticket)
+router.get('/ticket/:id', getTicketByTxRef)
 router.post('/webhook', flutterwaveWebhook)
-router.get('/all', getAllPayments)
-router.get('/tickets', getAllTickets)
 
 export default router
