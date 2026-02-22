f20a6ef284e9a03f5017c90dcbc57efff50e4ff3
Ogwara Precious <ogwaraprecious8@gmail.com>
2026-02-22 05:25:38 -0800

    fixing

--- app/payment-status/page.tsx
+++ app/payment-status/page.tsx
@@ -4,7 +4,7 @@ import PaymentStatusPageComponent from './../../components/paymentPage'
 export default function PaymentStatusPage() {
   return (
     <Suspense fallback={<div>Loading payment details...</div>}>
-      <PaymentStatusPageComponent />
+      <PaymentStatusPageComponent/>
     </Suspense>
   )
 }
