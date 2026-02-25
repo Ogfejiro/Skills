0b16023dab38e277f3bb612f3272661812a8d54d
Lucian Codes <ayomideayomipo147@gmail.com>
2026-02-25 17:38:10 +0100

    Crypto payment initiated

--- Backend Server/controller/payment.controller.js
+++ Backend Server/controller/payment.controller.js
@@ -1,9 +1,11 @@
 // payment.controller.js - COMPLETE WITH ALL FUNCTIONS
 import { flwClient } from '../config/flutterwave.js'
 import Payment from '../models/payment.model.js'
+import crypto from 'crypto'
 import Ticket from '../models/ticket.model.js'
 import { sendVerificationEmail } from '../services/sendVerificationEmail.js'
 import { generateTicket } from '../services/ticket.service.js'
+import { heleketService } from './../config/heleket.js'
 
 export const initiatePayment = async (req, res) => {
   try {
@@ -57,7 +59,11 @@ export const verifyPayment = async (req, res) => {
         .json({ success: false, error: 'Payment not found' })
     }
 
-    if (payment.status === 'successful') {
+    if (
+      payment.status === 'successful' ||
+      payment.status === 'paid' ||
+      payment.status === 'completed'
+    ) {
       return res.status(200).json({ success: true })
     } else {
       return res
@@ -125,5 +131,111 @@ export const getTicketByTxRef = async (req, res) => {
     res.json(ticket)
   } catch (err) {
     res.status(500).json({ error: 'Failed to retrieve ticket' })
+  }
+}
+
+export const initiateCryptoPayment = async (req, res) => {
+  try {
+    const { amount, email, userId, ticketName } = req.body
+
+    const tx_ref = 'tx-' + Date.now()
+
+    await Payment.create({
+      userId,
+      tx_ref,
+      amount,
+      currency: 'USDT',
+      status: 'pending',
+      customerEmail: email,
+      ticketName,
+    })
+
+    const paymentData = await heleketService({
+      amount,
+      currency: 'USDT',
+      order_id: tx_ref,
+      payer_email: email,
+      subtract: 100,
+      callback_url:
+        'https://skills-k6pv.onrender.com/api/payments/crypto-webhook',
+      return_url: `https://www.lofte.live/`,
+      url_success: `https://www.lofte.live/payment-status?tx_ref=${tx_ref}`,
+    })
+
+    console.log('Heleket response:', paymentData.result.url)
+
+    res.status(200).json({
+      success: true,
+      paymentLink: paymentData.result.url,
+    })
+  } catch (error) {
+    console.error('Initiate Error:', error.response?.data || error.message)
+    res.status(500).json({ success: false })
+  }
+}
+
+export const heleketWebhook = async (req, res) => {
+  try {
+    const data = { ...req.body }
+
+    const receivedSign = data.sign
+    if (!receivedSign) {
+      return res.status(400).json({ message: 'No signature' })
+    }
+
+    delete data.sign
+
+    const jsonString = JSON.stringify(data).replace(/\//g, '\\/')
+
+    const base64 = Buffer.from(jsonString).toString('base64')
+    const generatedSign = crypto
+      .createHash('md5')
+      .update(base64 + process.env.HELEKET_API_KEY)
+      .digest('hex')
+
+    if (generatedSign !== receivedSign) {
+      return res.status(400).json({ message: 'Invalid signature' })
+    }
+
+    const {
+      order_id,
+      status,
+      is_final,
+      merchant_amount,
+      txid,
+      network,
+      uuid,
+      payer_currency,
+    } = data
+
+    const order = await Payment.findOne({ tx_ref: order_id })
+    if (!order) return res.status(404).json({ message: 'Order not found' })
+
+    if (is_final && status === 'paid') {
+      // Prevent double processing
+      if (order.status !== 'paid') {
+        order.status = 'paid'
+        order.paymentReference = uuid
+        order.transactionId = txid
+        order.network = network
+        order.amount = merchant_amount
+        order.currency = payer_currency
+        await order.save()
+
+        await generateTicket(order.tx_ref)
+        await sendVerificationEmail(
+          order.customerEmail,
+          order.amount,
+          `https://www.lofte.live/tickets?tx_ref=${order.tx_ref}`,
+          order.ticketName,
+          order.tx_ref,
+        )
+      }
+    }
+
+    return res.status(200).json({ received: true })
+  } catch (error) {
+    console.error('Webhook Error:', error)
+    return res.status(500).json({ success: false, error: error.message })
   }
 }
--- Backend Server/routes/payment.routes.js
+++ Backend Server/routes/payment.routes.js
@@ -5,12 +5,16 @@ import {
   verifyPayment,
   flutterwaveWebhook,
   getTicketByTxRef,
+  initiateCryptoPayment,
+  heleketWebhook,
 } from '../controller/payment.controller.js'
 
 const router = express.Router()
 
 // Payment routes
 router.post('/initiate', initiatePayment)
+router.post('/pay-usdt', initiateCryptoPayment)
+router.post('/crypto-webhook', heleketWebhook)
 router.post('/verify', verifyPayment)
 router.post('/webhook', flutterwaveWebhook)
 router.get('/ticket/:tx_ref', getTicketByTxRef)
