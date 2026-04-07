7682a9c476eb0610c47648264a56434fff234398
Lucian Codes <ayomideayomipo147@gmail.com>
2026-04-07 01:34:03 +0100

    Event ticket loading

---
+++ Backend-Server/src/modules/events-ticket/ticket.service.js
@@ -0,0 +1,43 @@
+import EventTicket from '../../models/EventTicket.model.js'
+import Event from '../../models/Event.model.js'
+
+import AppError from './../../services/shared/appError.js'
+
+export async function createEventTicket(hostId, eventId, ticketData) {
+	const eventExist = await Event.findOne({ _id: eventId })
+	if (!eventExist) {
+		throw new AppError('Event not found', 404)
+	}
+	if (eventExist.hostId.toString() !== hostId) {
+		throw new AppError('Unauthorized to create ticket for this event', 403)
+	}
+
+	const newTicket = new EventTicket({
+		eventId,
+		...ticketData,
+	})
+	await newTicket.save()
+	return newTicket
+}
+
+export async function getTicketsByEvent(eventId) {
+	const eventExist = await Event.findOne({ _id: eventId })
+	if (!eventExist) {
+		throw new AppError('Event not found', 404)
+	}
+
+	const tickets = await EventTicket.find({ eventId })
+
+	return tickets
+}
+
+export async function editTicketById(ticketId, updateData) {
+	const ticket = await EventTicket.findByIdAndUpdate(ticketId, updateData, {
+		new: true,
+		runValidators: true,
+	})
+	if (!ticket) {
+		throw new AppError('Ticket not found', 404)
+	}
+	return ticket
+}
