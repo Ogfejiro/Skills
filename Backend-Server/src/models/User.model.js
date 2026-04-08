cb06cb78d52d16215cc487001fc47e840cb56f4e
Lucian Codes <ayomideayomipo147@gmail.com>
2026-04-08 23:07:53 +0100

    Fix DB error by changing the condition to check for phone number instead of email in auth.service.js

--- Backend-Server/src/models/User.model.js
+++ Backend-Server/src/models/User.model.js
@@ -1,65 +1,65 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            toLowerCase: true,
-        },
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+			toLowerCase: true,
+		},
 
-        password: {
-            type: String,
-            select: false,
-        },
+		password: {
+			type: String,
+			select: false,
+		},
 
-        phone: {
-            type: String,
-            unique: true,
-            required: true,
-        },
+		phone: {
+			type: String,
+			unique: true,
+			required: true,
+		},
 
-        refId: {
-            type: String,
-            required: true,
-            unique: true,
-        },
+		refId: {
+			type: String,
+			required: true,
+			unique: true,
+		},
 
-        firstName: {
-            type: String,
-        },
-        lastName: {
-            type: String,
-        },
-        profession: {
-            type: String,
-        },
-        role: {
-            type: String,
-            enum: ['User', 'Host', 'Admin'],
-            default: 'User',
-            unique: true,
-        },
+		firstName: {
+			type: String,
+		},
+		lastName: {
+			type: String,
+		},
+		profession: {
+			type: String,
+		},
+		role: {
+			type: String,
+			enum: ['User', 'Host', 'Admin'],
+			default: 'User',
+			required: true,
+		},
 
-        provider: {
-            type: [String],
-            enum: ['google', 'local'],
-            default: ['local'],
-        },
+		provider: {
+			type: [String],
+			enum: ['google', 'local'],
+			default: ['local'],
+		},
 
-        emailVerified: {
-            type: Boolean,
-            default: false,
-        },
+		emailVerified: {
+			type: Boolean,
+			default: false,
+		},
 
-        phoneVerified: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    { timestamps: true },
+		phoneVerified: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{ timestamps: true },
 )
 
 const User = mongoose.model('User', userSchema)
