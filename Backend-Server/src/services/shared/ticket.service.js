5df24e7dda9c37106f404bc77da7394f519e0c27
Lucian Codes <ayomideayomipo147@gmail.com>
2026-04-13 15:12:16 +0100

    Added Honey

--- Backend-Server/src/services/shared/ticket.service.js
+++ Backend-Server/src/services/shared/ticket.service.js
@@ -47,6 +47,8 @@ export async function generateRegularTicket(email, ticketName, eventName) {
 		return existingTicket
 	}
 
+	const now = new Date()
+
 	const monthYear =
 		String(now.getMonth() + 1).padStart(2, '0') +
 		String(now.getFullYear()).slice(-2)
